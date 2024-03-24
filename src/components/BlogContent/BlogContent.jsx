ab51c56ab553e04c8c51c8cdc737f3d00bda1be0
fokhrul2029 <ifokhrul.2029@gmail.com>
2024-03-24 23:47:52 +0600

    added blog posts

--- src/components/BlogContent/BlogContent.jsx
+++ src/components/BlogContent/BlogContent.jsx
@@ -1,12 +1,24 @@
+import { useEffect, useState } from "react";
 import Blogs from "../Blogs/Blogs";
 import Bookmarks from "../Bookmarks/Bookmarks";
 
 function BlogContent() {
+  const [blogs, setBlogs] = useState([]);
+  const [bookmarks, setBookmarks] = useState([]);
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setBlogs(data));
+  }, []);
   return (
     <section className="py-12">
       <div className="container mx-auto px-4">
-        <div className="grid grid-cols-3 gap-6">
-          <Blogs></Blogs>
+        <div className="flex flex-col-reverse lg:grid lg:grid-cols-3 gap-6">
+        <div className="col-span-2 flex flex-col gap-12">
+          {blogs.map((blog) => (
+            <Blogs blog={blog} key={blog.id} />
+          ))}
+          </div>
           <Bookmarks></Bookmarks>
         </div>
       </div>
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,9 +1,49 @@
-import Blog from "../Blog/Blog";
+/* eslint-disable react/prop-types */
+function Blogs({ blog }) {
+  const {
+    cover_img,
+    auth_img,
+    author_name,
+    publish_date,
+    reading_time,
+    title,
+    tags,
+  } = blog;
+  const targetDate = new Date(publish_date);
+  const today = new Date();
+  const diff = Math.abs(today - targetDate);
+  const diffInDays = Math.ceil(diff / (1000 * 60 * 60 * 24));
+  const date = diffInDays < 30 ? `(${diffInDays} days ago)` : "";
 
-function Blogs() {
   return (
-    <div className="col-span-2 flex flex-col gap-12">
-      <Blog></Blog>
+    <div className="flex flex-col gap-8 border-b-2 pb-2">
+      <img className="rounded-md" src={cover_img} alt="" />
+      <div className="flex justify-between items-center">
+        <div className="flex gap-4 items-center">
+          <img className="w-16" src={auth_img} alt="" />
+          <div className="flex flex-col gap-2">
+            <h2 className="text-2xl font-bold">{author_name}</h2>
+            <p className="text-base font-semibold">
+              {publish_date} {date}
+            </p>
+          </div>
+        </div>
+        <div className="flex gap-2 items-center font-medium text-xl">
+          <p>{reading_time} min read</p>
+          <img className="cursor-pointer" src="img/Frame.png" alt="" />
+        </div>
+      </div>
+      <h1 className="text-4xl font-bold">{title}</h1>
+      <div className="flex gap-3 text-xl font-medium">
+        {tags.map((tag, i) => (
+          <p key={i}>{tag}</p>
+        ))}
+      </div>
+      <div>
+        <button className="text-primaryColor font-semibold text-xl">
+          Mark as read
+        </button>
+      </div>
     </div>
   );
 }
